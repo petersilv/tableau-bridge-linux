93512180ed36f2851b27a6c9989b1caaacfdac8c
Peter Silvester <p.silvester.94@gmail.com>
2023-01-13 09:54:44 +0000

    Add deploy script for Tableau Bridge container

---
+++ bridge/deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Install Docker
+
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh && rm get-docker.sh
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Build Bridge Container and start Bridge
+
+wget https://raw.githubusercontent.com/petersilv/tableau-bridge-linux/main/bridge/Dockerfile
+
+docker image build -t bridge ./
+
+docker run -d \
+    --name bridge
+    --env-file ./variables \
+    bridge
