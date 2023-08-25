114dfc0b50695ee643032c6b409c80b281c98a8d
Peter Silvester <p.silvester.94@gmail.com>
2023-08-25 15:23:33 -0400

    Update bridge deploy script to remove wget of Dockerfile

--- bridge/deploy.sh
+++ bridge/deploy.sh
@@ -11,8 +11,6 @@ sudo sh get-docker.sh && rm get-docker.sh
 # ----------------------------------------------------------------------------------------------------------------------
 # Build Bridge Container and start Bridge
 
-wget https://raw.githubusercontent.com/petersilv/tableau-bridge-linux/main/bridge/Dockerfile
-
 docker image build -t bridge ./
 
 docker run -d \
