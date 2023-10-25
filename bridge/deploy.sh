e903e9eb4b79e77d2491470d207c2e7660777219
Peter Silvester <p.silvester.94@gmail.com>
2023-10-25 11:37:24 +0100

    Include the download of the bridge installer as part of the deploy script

--- bridge/deploy.sh
+++ bridge/deploy.sh
@@ -9,6 +9,11 @@ curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh && rm get-docker.sh
 
 # ----------------------------------------------------------------------------------------------------------------------
+# Download Bridge Installer
+
+wget -O bridge.rpm https://downloads.tableau.com/tssoftware/TableauBridge-20233.23.1017.0948.x86_64.rpm
+
+# ----------------------------------------------------------------------------------------------------------------------
 # Build Bridge Container and start Bridge
 
 docker image build -t bridge ./
